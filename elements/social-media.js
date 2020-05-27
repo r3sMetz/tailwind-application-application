68f646b956812c07c460f84b787a7863a12d7927
Sascha Metz <metz@raum3.de>
2020-05-28 01:50:21 +0200

    Add social-media hover

--- elements/social-media.js
+++ elements/social-media.js
@@ -10,6 +10,7 @@ export const SocialMedia = ({ className }) => {
           target="_blank"
           rel="noopener noreferrer"
           href="https://github.com/r3sMetz/tailwind-application-application"
+          className="hover:text-gray-700"
         >
           <Icon name="github" className="w-5 h-auto" />
         </a>
@@ -19,6 +20,7 @@ export const SocialMedia = ({ className }) => {
           target="_blank"
           rel="noopener noreferrer"
           href="https://twitter.com/tailwindcss"
+          className="hover:text-gray-700"
         >
           <Icon name="twitter" className="w-5 h-auto" />
         </a>
@@ -28,6 +30,7 @@ export const SocialMedia = ({ className }) => {
           target="_blank"
           rel="noopener noreferrer"
           href="https://discord.com/invite/7NF8GNe"
+          className="hover:text-gray-700"
         >
           <Icon name="discord" className="w-5 h-auto" />
         </a>
