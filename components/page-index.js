921af655bbb81e9b5138c90cef3cbf8aabf2baad
Sascha Metz <metz@raum3.de>
2020-05-28 02:31:27 +0200

    Add TailwindUI Ad to page-index

--- components/page-index.js
+++ components/page-index.js
@@ -3,11 +3,11 @@ import React from "react"
 
 export const PageIndex = ({ className, links }) => {
   return (
-    <nav className={`h-64 text-sm text-gray-600 ${className}`}>
+    <div className={`text-sm text-gray-600 ${className}`}>
       <h5 className="text-xs mb-2 text-gray-500 uppercase font-semibold">
         On this Page
       </h5>
-      <ul>
+      <ul className="mb-10">
         {links.map((link, key) => (
           <li key={key}>
             <a
@@ -19,7 +19,25 @@ export const PageIndex = ({ className, links }) => {
           </li>
         ))}
       </ul>
-    </nav>
+      <article>
+        <img
+          className="w-full h-auto"
+          src="/tailwind-ui-sidebar.png"
+          alt="TailwindUI"
+        />
+        <p className="my-2">
+          Beautiful UI components by the creators of Tailwind CSS.
+        </p>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          className="text-gray-700 font-semibold hover:underline"
+          href="https://tailwindui.com"
+        >
+          Learn more &rarr;
+        </a>
+      </article>
+    </div>
   )
 }
 
